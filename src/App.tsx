81c369560ce469cd1da5d31da38d51878de7489f
Islomjon <ergashevislomjon1020@gmail.com>
2024-10-02 22:41:33 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'jadid-ui-test'
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <Button type='primary' shape='circle' className='mt-10 ml-10'>Click me</Button>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+     './node_modules/jadid-ui-test/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
